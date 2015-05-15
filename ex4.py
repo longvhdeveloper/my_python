d7a5d4dd9b0a0debf628f5b0f0c2c247eaa6850e
longvo <jackie@vietnamcubator.com>
2015-05-15 15:58:52 +0700

    done exercise 3 and 4

---
+++ ex4.py
@@ -0,0 +1,14 @@
+cars = 100
+space_in_a_car = 4.0
+drivers = 30
+passengers = 90
+cars_not_driven = cars - drivers
+car_driven = drivers
+carpool_capacity = car_driven * space_in_a_car
+average_passengers_per_car = passengers / car_driven
+
+print "There are", cars, "cars available"
+print "There are only", drivers, "drivers available"
+print "We can transport", carpool_capacity, "people"
+print "We have", passengers, "to carpool today"
+print "We need to put about", average_passengers_per_car, "in each cars"
